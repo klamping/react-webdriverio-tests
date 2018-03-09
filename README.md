12eff121dea9f3153f65d597c07fec0bef2cdfa6
Kevin Lamping <klamping@gmail.com>
2018-03-09 17:25:00 -0600

    get local example working

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# wdio-react-example
+# DOM Test Fixture Automation
+
+An experiment to automate manual testing of the [ReactDOM test fixtures](https://github.com/facebook/react/tree/master/fixtures/dom).
+
+## Setup
+
+This setup has only been tested in High Sierra OSX.
+
+```bash
+yarn install
+cp .env.example .env
+```
+
+### Local testing
+
+Run:
+
+```bash
+yarn test
+```
+
+By default, this tests Chrome locally.
+
+### Local Server Testing
+
+To define a local server to test again, pass in a 'baseUrl' setting:
+
+```bash
+yarn test --baseUrl=http://localhost:5000
+```
+
+### Remote testing
+
+1. Create an account (a free trial is fine) on [Browserstack](http://browserstack.com) or [Sauce Labs](https://saucelabs.com/).
+2. Copy `.env.example` to `.env`.
+3. Fill in your browserstack username and key within `.env`
+
+```bash
+yarn test
+```
+
+By default, this tests 5 different browsers on the Browserstack platform.
---
+++ test/example.test.js
@@ -0,0 +1,25 @@
+describe("Input change events", function () {
+    it("When a radio value and name changes at once", function() {
+        browser.url("/input-change-events")
+        browser.waitForVisible(".container")
+
+        // Save the button that has text of 'toggle'
+        const toggleButton = $("button*=Toggle");
+
+        // Save the radio buttons by their text using Xpath
+        const firstRadio = $("//label[text()='First Radio']/input");
+        const secondRadio = $("//label[text()='Second Radio']/input");
+
+        toggleButton.click()
+        expect(firstRadio.isSelected()).to.be.true;
+        expect(secondRadio.isSelected()).to.be.false;
+
+        toggleButton.click()
+        expect(firstRadio.isSelected()).to.be.false;
+        expect(secondRadio.isSelected()).to.be.true;
+
+        toggleButton.click()
+        expect(firstRadio.isSelected()).to.be.true;
+        expect(secondRadio.isSelected()).to.be.false;
+    });
+});
